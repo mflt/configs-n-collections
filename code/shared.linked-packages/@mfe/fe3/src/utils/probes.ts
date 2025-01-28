f0d414adee53abea3ac9a2762dbd598190ca27b0
timur x <timurx@eezza.dev>
2025-01-28 03:24:48 +0100

    _feIsAsyncFunction args type

--- code/shared.linked-packages/@mfe/fe3/src/utils/probes.ts
+++ code/shared.linked-packages/@mfe/fe3/src/utils/probes.ts
@@ -1,13 +1,16 @@
 
 export const _feIsNumber = (num: number): num is number => +num === +num;
 export const _feIsString = (that: unknown): that is string|String => typeof that === 'string' || that instanceof String;
-export const _feIsMap = <K, V>(that: unknown): that is Map<K, V> => that instanceof Map;
-export const _feIsWeakMap = <K extends Object, V>(that: unknown): that is WeakMap<K, V> => that instanceof WeakMap;
-export const _feIsArray = <T>(that: unknown): that is Array<T> => that instanceof Array;
-export const _feIsSet = <T>(that: unknown): that is Set<T> => that instanceof Set;
+export const _feIsMap = <K, V> (that: unknown): that is Map<K, V> => that instanceof Map;
+export const _feIsWeakMap = <K extends Object, V> (that: unknown): that is WeakMap<K, V> => that instanceof WeakMap;
+export const _feIsArray = <T> (that: unknown): that is Array<T> => that instanceof Array;
+export const _feIsSet = <T> (that: unknown): that is Set<T> => that instanceof Set;
 export const _feIsObject = (that: unknown): that is Object => that instanceof Object;
 export const _feIsFunction = (that: unknown): that is Function => that instanceof Function; // @TODO all to unknown
-export const _feIsAsyncFunction = <P = unknown>(that: unknown): that is ()=>Promise<P> => that?.constructor?.name === 'AsyncFunction'; // @TODO return type ? probably done
+export const _feIsAsyncFunction = <
+  P = unknown,
+  Args extends any[] = any[]
+> (that: unknown): that is (...args: Args)=>Promise<P> => that?.constructor?.name === 'AsyncFunction'; // @TODO return type ? probably done
 
 export const _feIsIterable = <T>(that: unknown): that is Iterable<T> => (
   !!that
