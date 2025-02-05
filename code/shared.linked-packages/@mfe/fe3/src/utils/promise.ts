7a1d5511e2b6ad77102b4ae518ce2283af2ed2c5
timur x <timurx@eezza.dev>
2025-02-05 23:50:32 +0100

    FeExecSignaling

--- code/shared.linked-packages/@mfe/fe3/src/utils/promise.ts
+++ code/shared.linked-packages/@mfe/fe3/src/utils/promise.ts
@@ -9,12 +9,11 @@ type _Reject <
 > =
   (reason?: ErrorReasonT) => void  // Parameters<ConstructorParameters<typeof Promise<FulfillmentValueT>>[0]>[1]
 
-
 export class FePromisewithResolvers <
   FulfillmentValueT = true,
   ErrorReasonT = unknown
-> extends Promise<FulfillmentValueT>
-{
+> extends Promise<FulfillmentValueT> {
+
   static get [Symbol.species]() { // @TODO
     return Promise;
   }
@@ -22,7 +21,7 @@ export class FePromisewithResolvers <
   public resolve!: _Resolve<FulfillmentValueT>
   public reject!: _Reject<ErrorReasonT>
 
-  public constructor() {
+  public constructor () {
     let resolve_reject: [_Resolve<FulfillmentValueT>, _Reject<ErrorReasonT>]
     super((resolve, reject) => {
       resolve_reject = [resolve, reject]
@@ -31,13 +30,28 @@ export class FePromisewithResolvers <
     this.reject = resolve_reject![1]
   }
 }
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/withResolvers
 // https://shaky.sh/promise-with-resolvers/
+
+export class FeExecSignaling <
+  FulfillmentValueT = true,
+  ErrorReasonT = unknown
+> extends FePromisewithResolvers<FulfillmentValueT,ErrorReasonT> {
+  public get requested () { return this.promise as Promise<true> }
+  public done (value?: FulfillmentValueT) { this.resolve(value || true as FulfillmentValueT) }
+  public fail (err: ErrorReasonT) { this.reject(err)}
+  public constructor () {
+    super()
+  }
+}
+
+
 
 export class DeferredPromise extends Promise<void> {
   static get [Symbol.species]() {
     return Promise;
   }
-  constructor() {
+  constructor () {
     let internalResolve = () => { };
     let internalReject = () => { };
     super((resolve, reject) => {
