d10da0b1a23bd16429404d9c666cf5d9dc593370
timur x <timurx@eezza.dev>
2025-02-06 03:34:49 +0100

    FeReadinessSignaling

--- code/shared.linked-packages/@mfe/fe3/src/utils/promise.ts
+++ code/shared.linked-packages/@mfe/fe3/src/utils/promise.ts
@@ -33,29 +33,38 @@ export class FePromisewithResolvers <
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/withResolvers
 // https://shaky.sh/promise-with-resolvers/
 
-export class FeExecSignaling <
-  FulfillmentValueT = true,
-  ErrorReasonT = unknown
-> extends FePromisewithResolvers<FulfillmentValueT,ErrorReasonT> {
-  public get requested () { return this.promise as Promise<FulfillmentValueT> }
-  public done (value?: FulfillmentValueT) { this.resolve(value || true as FulfillmentValueT) }
-  public fail (err: ErrorReasonT) { this.reject(err)}
-  public constructor () {
-    super()
-  }
-}
 
 export class FeReadinessSignaling <
   FulfillmentValueT = true,
   ErrorReasonT = unknown
 > extends FePromisewithResolvers<FulfillmentValueT,ErrorReasonT> {
-  public get wakeonReady () { return this.promise as Promise<FulfillmentValueT> }
+  public get tillPassed () { return this.promise as Promise<FulfillmentValueT> }
   public pass (value?: FulfillmentValueT) { this.resolve(value || true as FulfillmentValueT) }
   public fail (err: ErrorReasonT) { this.reject(err)}
   public constructor () {
     super()
   }
 }
+
+
+export class FeExecSignaling <
+  ExecutionValueT = true,
+  RequestedValueT = true,
+  ErrorReasonT = unknown
+> extends FePromisewithResolvers<ExecutionValueT,ErrorReasonT> {
+  private _requested: PromiseWithResolvers<RequestedValueT>
+  public get tillRequested () { return this._requested.promise as Promise<RequestedValueT> }
+  public request (value?: RequestedValueT) { this._requested.resolve(value || true as RequestedValueT) }
+  public skip (err: ErrorReasonT) { this._requested.reject(err)}  // @TODO maybe different typing
+  public get tillDone () { return this.promise as Promise<ExecutionValueT> }
+  public done (value?: ExecutionValueT) { this.resolve(value || true as ExecutionValueT) }
+  public fail (err: ErrorReasonT) { this.reject(err)}
+  public constructor () {
+    super()
+    this._requested = Promise.withResolvers<RequestedValueT>()
+  }
+}
+
 
 // Just classic:
 
